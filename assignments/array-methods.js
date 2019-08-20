c0085ab5abecb0737c8e4f1df7f4a4689f73c3c4
Devanee Reid <devaneereid@gmail.com>
2019-08-20 17:09:33 -0600

    answered challenge

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -57,11 +57,10 @@ const runners = [
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
-let fullNames = [0];
+let fullNames = [];
 
-fullNames.forEach(function(element){
-console.log(element);
-fullNames++;
+fullNames = fullNames.forEach(function(item){
+return item.first_name + item.last_name;
 });
 
 console.log(fullNames);
@@ -69,11 +68,17 @@ console.log(fullNames);
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
 let firstNamesAllCaps = [];
+firstNamesAllCaps = runners.map(function(item){
+  return item.first_name.toUpperCase();
+});
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
 let runnersLargeSizeShirt = [];
+runnersLargeSizeShirt = runners.filter(function(){
+  return item.shirt_size;
+}
 
 console.log(runnersLargeSizeShirt.filter);
 
