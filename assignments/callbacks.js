040fa72a2903b20ff59f9dbf2737cc40959d68c5
Devanee Reid <devaneereid@gmail.com>
2019-08-20 14:36:45 -0600

    answering questions in callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -40,26 +40,38 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
+
   // getLength passes the length of the array into the callback.
 }
+console.log(items.length);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
+console.log(last.length);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+return x + y;
 }
+console.log(sumNums(2,1));
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+
 }
+console.log(3 * 5);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if(list.includes(item)){
+    return cb(true)
+  }
+  return cb(false)
 }
-
+console.log();
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
